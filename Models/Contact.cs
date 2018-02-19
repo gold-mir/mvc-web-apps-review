a5ffcaaeac0266a36370c8266d15a39a4061b41b
gold-mir <miranda.keith.42@gmail.com>
2018-02-18 18:21:05 -0800

    add contact class

---
+++ Models/Contact.cs
@@ -0,0 +1,78 @@
+using System.Collections.Generic;
+
+namespace Application.Models
+{
+    public class Contact
+    {
+        private static List<Contact> allContacts = new List<Contact>();
+        private static int maxID = 0;
+
+        public static Contact GetByID(int id)
+        {
+            foreach(Contact c in allContacts)
+            {
+                if (c.GetID() == id)
+                {
+                    return c;
+                }
+            }
+
+            return null;
+        }
+
+        public static List<Contact> GetAllInstances()
+        {
+            return allContacts;
+        }
+
+        private string _name;
+        private string _email;
+        private string _phone;
+        private int _id;
+
+        public Contact(name, email, phone)
+        {
+            _name = name;
+            _email = email;
+            _phone = phone;
+
+            allContacts += this;
+            _id = maxID++;
+        }
+
+        public int GetID()
+        {
+            return _id;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public string GetEmail()
+        {
+            return _email;
+        }
+
+        public string GetPhone()
+        {
+            return _phone;
+        }
+
+        public void SetName(string name)
+        {
+            _name = name;
+        }
+
+        public void SetEmail(string email)
+        {
+            _email = email;
+        }
+
+        public void SetPhone(string phone)
+        {
+            _phone = phone;
+        }
+    }
+}
